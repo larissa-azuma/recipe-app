5e9a9ef3bd44cd362ae1feb6b1c44b8d180c94ca
linda azuma <azumalinda29@gmail.com>
2024-03-19 07:58:22 +0000

    updated changes

--- src/pages/recipe/index.jsx
+++ src/pages/recipe/index.jsx
@@ -1,6 +1,38 @@
 import { useParams } from "react-router-dom";
+import useSWR from "swr";
+import loadingIcon from "../../assets/images/infinite-spinner.svg";
+import { Container } from "@mui/material";
+
+const getRecipe =(...args) => {
+  //prepare URL
+  const url = new URL(args[0]);
+  url.searchParams.append(
+    "apiKey", process.env.REACT_APP_SPOONACULAR_API_KEY);
+   // fetch and return recipe
+   return fetch(url) .then(response => response.json()) 
+}
 
 export default function Recipe() {
   const { id } = useParams();
-  return <h1>Recipe Page: {id}</h1>;
+  const {data,isLoading} = useSWR(`https://api.spoonacular.com/recipes/${id}/information`, getRecipe);
+  //console.log(data,isLoading);
+
+  return (
+  <>
+     {isLoading ? <img src ={loadingIcon} /> :(
+
+      <Container>
+        <h1> {data.title} </h1>
+
+      <div dangerouslySetInnerHTML =  {{__html:data.summary}} />
+        <img src ={data.image} />
+        
+      </Container>
+
+      )
+     }
+     
+    </>
+  )
+  
 }
